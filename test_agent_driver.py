3aa8ec4f9c8cd983587141f54a57918b330f3c99
CeeEffEff <cfehilly1337@hotmail.co.uk>
2021-07-16 17:57:07 +0100

    Periodic pause

--- test_agent_driver.py
+++ test_agent_driver.py
@@ -12,6 +12,7 @@ driver = AgentDriver(num_collect_episodes=10, num_eval_episodes= 4, replay_buffe
 
 
 input_bool = True
+input_frequency = 10
 evaluate_before_train = True
 
 
@@ -32,22 +33,24 @@ else:
     #input()
 
 
-def pause_input(message:str):
-    if input_bool:
+def pause_input(message:str, iteration:int):
+    if input_frequency == 0:
+        return
+    if iteration % input_frequency == 0 and input_bool:
         input(message)
 
 for i in range(200):
-    pause_input("Press ENTER to explore using collect policy")
+    pause_input("Press ENTER to explore using collect policy", i)
     print("Iteration", i)
     print("Exploring...")
     driver.run_collect(verbose=True)
 
     if (evaluate_before_train):
-        pause_input("Press ENTER to evaluate target before training")
+        pause_input("Press ENTER to evaluate target before training", i)
         print("Before training, evaluating target...")
         driver.run_target(verbose=True)
 
-    pause_input("Press ENTER to train")
+    pause_input("Press ENTER to train", i)
     print("Training...")
     #driver.train_target(train_steps=40, sample_batch_size=10, verbose=True)
     #driver.train_target(train_steps=100, sample_batch_size=64, verbose=True)
@@ -55,8 +58,9 @@ for i in range(200):
     #driver.train_target_on_all(verbose=True)
     print()
 
-    pause_input("Press ENTER to evaluate target after training")
+    pause_input("Press ENTER to evaluate target after training", i)
     print("Evaluating target...")
     driver.run_target(verbose=True)
+    pause_input("Press ENTER continue after the above evaluation", i)
 
 input("Completed")
